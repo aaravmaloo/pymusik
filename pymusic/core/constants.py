4b3df7470470fa9887e503a06648b72912cc195e
Aarav Maloo <aaravmaloo06@gmail.com>
2026-01-28 19:30:46 +0530

    create contants for defining basic scales and things that make up music

---
+++ pymusic/core/constants.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+A4_FREQ = 440.0
+SEMITONE_RATIO = 2 ** (1 / 12)
+
+DEFAULT_SAMPLERATE = 44100
+DEFAULT_BPM = 120.0
+DEFAULT_TIME_SIGNATURE = (4, 4)
+
+MAX_VELOCITY = 1.0
+
+SCALES = {
+    "major": [0, 2, 4, 5, 7, 9, 11],
+    "minor": [0, 2, 3, 5, 7, 8, 10],
+    "dorian": [0, 2, 3, 5, 7, 9, 10],
+    "phrygian": [0, 1, 3, 5, 7, 8, 10],
+    "lydian": [0, 2, 4, 6, 7, 9, 11],
+    "mixolydian": [0, 2, 4, 5, 7, 9, 10],
+    "locrian": [0, 1, 3, 5, 6, 8, 10],
+    "pentatonic_major": [0, 2, 4, 7, 9],
+    "pentatonic_minor": [0, 3, 5, 7, 10],
+}
+
+NOTE_NAMES = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+FLAT_NAMES = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"]
