93f6989d3f44d679f5f2a774fda07a61e6faa431
Aarav Maloo <aaravmaloo06@gmail.com>
2026-04-19 10:32:57 +0530

    update init.py to initialze the latest intruments

---
+++ pymusik/instruments/__init__.py
@@ -0,0 +1,17 @@
+from .base import Instrument
+from .synth import SynthInstrument
+from .piano import PianoInstrument
+from .drums import DrumInstrument
+from .phonk import PhonkCowbell, Bass808, DarkPad
+from .lofi import GuitarInstrument, SaxInstrument, VinylEffect, MellowPiano
+from .pro import AcidBass, ProDrums, SuperSawLead, HyperPluck, AnalogLead, AtmosphericStrings
+from .strings import Violin, Viola, Cello, Contrabass, Harp
+from .brass import Trumpet, Trombone, FrenchHorn, Tuba
+from .woodwinds import Flute, Piccolo, Clarinet, Oboe, Bassoon, Recorder
+from .percussion import Timpani, Marimba, Xylophone, Vibraphone, Gong, Tambourine, Shaker, Conga, Bongo, Tabla
+from .organ import PipeOrgan, HammondOrgan, Accordion, Harmonium
+from .bass import FretlessBass, UprightBass, SubBass, WobbleBass
+from .electronic import TB303, SupersawPad, WavetableLead, FMBass, TranceGate, ChiptuneLead, ChiptuneBass
+from .folk import Banjo, Mandolin, Ukulele, Sitar, Duduk, Bagpipes, HurdyGurdy
+from .keys import Clavinet, Celesta, Glockenspiel, MusicBox, Rhodes
+from .choir import ChoirPad, VocalOoh, VocalAah
